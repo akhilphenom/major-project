8a6c27e13e1fd8bbf16efb2f0cd60ff2a1510932
akhilphenom <saiakhilkatukam@inncircles.com>
2022-05-30 02:32:19 +0530

    build test

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -6,12 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent implements OnInit {
-
+  public finalResult;
+  public classes = {
+    'akiec' : 0, 'bcc' :1, 'bkl': 2, 'df': 3, 'mel' : 4,'nv' : 5,"sk": 6,"scc":7, 'vasc' : 8
+  };
   constructor() { }
 
   ngOnInit(): void {
   }
   public getPredictions(event){
-    console.log(event);
+    var max = [Number.MIN_VALUE,-1];
+    event.forEach((element,i=0) => {
+      if(element>max[0]){
+        max=[element,i]
+      }
+      i=i+1;
+    });
+    this.finalResult = [Object.keys(this.classes).find(key => this.classes[key]==max[1]),max[0]];
   }
 }
